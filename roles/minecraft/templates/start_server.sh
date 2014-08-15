4aa58aeee498ed509e05d560778a51a53e4589f6
Chris Joel <chris@scriptolo.gy>
2014-08-14 21:49:15 -0700

    Make it possible to override JVM args.

--- roles/minecraft/templates/start_server.sh
+++ roles/minecraft/templates/start_server.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+set -x
+
+if [ -z "$jvm_args" ]
+then
+  jvm_args="-Xmx1024M -Xms1024M"
+fi
 
 cd {{ minecraft_root }}/volume
 ln -fs {{ minecraft_root }}/eula.txt
-java -Xmx1024M -Xms1024M -jar {{ minecraft_root }}/minecraft_server.{{ minecraft_version }}.jar
+java $jvm_args -jar {{ minecraft_root }}/minecraft_server.{{ minecraft_version }}.jar
+set +x
